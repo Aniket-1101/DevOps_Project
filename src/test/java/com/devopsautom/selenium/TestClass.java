4424a854e9dfc279a8dc65b9286155c508fd9a35
Aniket-1101 <aniketinamdar91@gmail.com>
2025-02-07 19:25:45 +0530

    changing test class

--- src/test/java/com/devopsautom/selenium/TestClass.java
+++ src/test/java/com/devopsautom/selenium/TestClass.java
@@ -1,9 +1,15 @@
 package com.devopsautom.selenium;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,16 +19,20 @@ public class TestClass {
 	public static WebDriver driver;
 	
 	@BeforeMethod
-	public void launchDriver() {
+	public void launchDriver() throws MalformedURLException {
+		ChromeOptions chromeOptions = new ChromeOptions();
+//		chromeOptions.setCapability("browserVersion", "100");
+		chromeOptions.setCapability("platformName", "Windows");
 		System.setProperty("webdriver.chrome.driver","C:\\TestingDevops\\selenium\\chromedriver.exe");
-		driver = new ChromeDriver();
+//		driver = new ChromeDriver();
+		driver = new RemoteWebDriver(new URL("http://192.168.16.1:4444"), chromeOptions);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().fullscreen();
 	}
 	
 	@Test
 	public void test1() {
-		driver.get("https://www.google.com/");
+		driver.get("https://www.facebook.com/");
 		System.out.println(driver.getTitle());
 	}
 	
